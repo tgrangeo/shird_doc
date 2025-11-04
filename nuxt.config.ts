90fc2f58f6a30f4130146923fe467e13bd54da70
tgrangeo <t.grangeon@xefi.fr>
2025-11-04 09:31:51 +0100

    deploy/4 try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,24 @@
 export default defineNuxtConfig({
-    extends: ['docus']
+    extends: ['docus'],
+    app: {
+        baseURL: process.env.NUXT_APP_BASE_URL || '/',
+        buildAssetsDir: '_nuxt/', 
+    },
+
+    nitro: {
+        preset: 'static',
+        output: {
+            publicDir: '.output/public'
+        }
+    },
+
+    experimental: {
+        payloadExtraction: false
+    },
+
+    router: {
+        options: {
+            hashMode: false
+        }
+    }
 })
